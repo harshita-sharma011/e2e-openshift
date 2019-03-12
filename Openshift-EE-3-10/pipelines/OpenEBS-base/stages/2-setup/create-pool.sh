818e02343465d297ba9cf9e7acb6829123004eb0
nsathyaseelan <sathyaseelan.n@mayadata.io>
2019-03-12 15:43:54 +0530

    Included storage policies script into pipeline
    Signed-off-by: nsathyaseelan <sathyaseelan.n@mayadata.io>

--- Openshift-EE-3-10/pipelines/OpenEBS-base/stages/2-setup/create-pool.sh
+++ Openshift-EE-3-10/pipelines/OpenEBS-base/stages/2-setup/create-pool.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
+set -x
+pod() {
+sshpass -p $pass ssh -o StrictHostKeyChecking=no $user@$ip 'git clone https://github.com/openebs/e2e-openshift.git'
 
-echo "Creating cstor pool"
+sshpass -p $pass ssh -o StrictHostKeyChecking=no $user@$ip 'cd e2e-openshift && git checkout rigorous && bash Openshift-EE-3-10/pipelines/OpenEBS-base/stages/2-setup/create-pool.sh node '"'$CI_JOB_ID'"''
+
+}
+
+node() {
+
+job_id=$(echo $1)
+
+#pooling over previous job to complete
+echo "*******Creating cstor-pool**************"
+bash Openshift-EE-3-10/utils/pooling jobname:s2-j2-policies
+bash Openshift-EE-3-10/utils/e2e-cr jobname:s2-j3-cstor-pool jobphase:Running
+sleep 30
+bash Openshift-EE-3-10/utils/e2e-cr jobname:s2-j3-cstor-pool jobphase:Completed
+
+}
+
+if [ "$1" == "node" ];then
+  node $2
+else
+  pod
+fi
--- Openshift-EE-3-10/pipelines/OpenEBS-base/stages/2-setup/deploy-openebs.sh
+++ Openshift-EE-3-10/pipelines/OpenEBS-base/stages/2-setup/deploy-openebs.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
+set -x
 
-echo "Deploying openebs"
+pod() {
+echo $CI_JOB_ID
+###clone e2e-openshift-repo
+echo "cloning e2e-openshift repo*************"
+sshpass -p $pass ssh -o StrictHostKeyChecking=no $user@$ip 'git clone https://github.com/openebs/e2e-openshift.git'
+sshpass -p $pass ssh -o StrictHostKeyChecking=no $user@$ip 'cd e2e-openshift && git checkout rigorous && bash Openshift-EE-3-10/pipelines/OpenEBS-base/stages/2-setup/deploy-openebs.sh node '"'$CI_JOB_ID'"''
+}
+
+node() {
+job_id=$(echo $1)
+
+echo "*****Deploying openebs*******"
+
+bash Openshift-EE-3-10/utils/e2e-cr jobname:s2-j1-openebs-deploy jobphase:Running 
+bash Openshift-EE-3-10/utils/e2e-cr jobname:s2-j2-policies jobphase:Waiting
+bash Openshift-EE-3-10/utils/e2e-cr jobname:s2-j3-cstor-pool jobphase:Waiting
+sleep 30
+bash Openshift-EE-3-10/utils/e2e-cr jobname:s2-j1-openebs-deploy jobphase:Completed
+
+}
+
+#"$@"
+
+if [ "$1" == "node" ];then
+  node $2
+else
+  pod
+fi
